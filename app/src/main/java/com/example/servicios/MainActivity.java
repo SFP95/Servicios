5a37b6667d940d0520c42ae2f607b95e7a85ac2e
Nia <103027252+SFP95@users.noreply.github.com>
2023-02-21 14:12:44 +0100

    21.02.23

--- app/src/main/java/com/example/servicios/MainActivity.java
+++ app/src/main/java/com/example/servicios/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.servicios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLanzar(View view) {
+        Intent i = new Intent(this, MyService.class);
+        startService(i);
+    }
+
+    public void onDetener(View view) {
+        Intent i = new Intent(this, MyService.class);
+        stopService(i);
     }
 }
--- app/src/main/java/com/example/servicios/MyService.java
+++ app/src/main/java/com/example/servicios/MyService.java
@@ -3,14 +3,39 @@ package com.example.servicios;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
+    private  static  final String TAG = "PDMD";
+
     public MyService() {
     }
 
     @Override
+    public void onCreate() {
+        Log.i(TAG,"onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG,"onStartCommand (intente, flags, starId): ("+ intent+"-"+flags+"-"+startId+")");
+        int i =0;
+        while (i==0){
+            //bucle infinito, si aplicamos un true, el return se queja y de esta forma lo engañamos
+        }
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG,"onDestroy");
+
+        super.onDestroy();
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
+        Log.i(TAG,"onBind");
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
