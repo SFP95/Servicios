401c376c7bd377dcda71d423631eaad9a87d542d
Nia <88581938+NiaD95@users.noreply.github.com>
2023-02-23 10:12:22 +0100

    23.02.23

--- app/src/main/java/com/example/servicios/MainActivity.java
+++ app/src/main/java/com/example/servicios/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.servicios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +24,11 @@ public class MainActivity extends AppCompatActivity {
     public void onDetener(View view) {
         Intent i = new Intent(this, MyService.class);
         stopService(i);
+    }
+    public void onPulsar(View v) {
+        Intent i = new Intent(this,MyService.class);
+        Button b= (Button) v;
+        i.putExtra("etiqueta", b.getText().toString());
+        startService(i);
     }
 }
--- app/src/main/java/com/example/servicios/MyService.java
+++ app/src/main/java/com/example/servicios/MyService.java
@@ -1,9 +1,60 @@
 package com.example.servicios;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.media.ToneGenerator;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+
+public class MyService extends IntentService {
+    private  static  final String TAG = "PDMD";
+    private ToneGenerator _tg;
+    protected final static int DURACION = 500;
+    public MyService(String name) {
+        super(name);
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG,"onCreate");
+    }
+    @Override
+    public void onDestroy() {
+        Log.i(TAG,"onDestroy");
+
+        super.onDestroy();
+    }
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.i(TAG,"onHandleIntent"+ intent);
+        String etiqueta = intent.getStringExtra("etiqueta");
+        play(etiqueta);
+    }
+
+    private void play(String etiqueta) {
+        int tono = 0;
+        switch (etiqueta.charAt(0)){
+            case '#': tono = ToneGenerator.TONE_DTMF_P; break;
+            case '*': tono = ToneGenerator.TONE_DTMF_S; break;
+            default: tono = ToneGenerator.TONE_DTMF_0 + (etiqueta.charAt(0) - '0'); break;
+        }
+        _tg.startTone(tono,DURACION);
+
+        try {
+            Thread.sleep(DURACION);
+        }catch (InterruptedException e){
+            throw  new RuntimeException();
+        }
+    }
+}
+
+
+
+/*
 
 public class MyService extends Service {
     private  static  final String TAG = "PDMD";
@@ -38,4 +89,4 @@ public class MyService extends Service {
         Log.i(TAG,"onBind");
         throw new UnsupportedOperationException("Not yet implemented");
     }
-}
+}*/
