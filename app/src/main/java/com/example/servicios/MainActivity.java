84c97c94f47cececa6da54c7e1e1836599c84a29
Nia <88581938+NiaD95@users.noreply.github.com>
2023-02-23 11:01:39 +0100

    23.02.23

--- app/src/main/java/com/example/servicios/MainActivity.java
+++ app/src/main/java/com/example/servicios/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.servicios;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver _br;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //ej 3
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _br = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                procesarTeclaPulsada(intent);
+            }
+        };
+
+        //filtro de intents
+
+        IntentFilter filtro = new IntentFilter(getClass().getName());
+        LocalBroadcastManager.getInstance(this).registerReceiver(_br, filtro);
+    }
+
+    private void procesarTeclaPulsada(Intent intent) {
+        String tecla = intent.getStringExtra("tecla");
+        TextView tvPantalla = findViewById(R.id.tvPantalla);
+        tvPantalla.append(intent.getStringExtra("tecla"));
+    }
+
+    //ADD
+    public static void addKey(Context c, String key) {
+        Intent i = new Intent(MainActivity.class.getName());
+        i.putExtra("tecla", key);
+        LocalBroadcastManager.getInstance(c).sendBroadcast(i);
+    }
+
+    public void onPulsar(View v) {
+        Intent i = new Intent(this,MyService.class);
+        Button b= (Button) v;
+        i.putExtra("etiqueta", b.getText().toString());
+        startService(i);
+    }
+
+
+
+
+/* // ejs 1 y 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +82,5 @@ public class MainActivity extends AppCompatActivity {
         Button b= (Button) v;
         i.putExtra("etiqueta", b.getText().toString());
         startService(i);
-    }
+    }*/
 }
--- app/src/main/java/com/example/servicios/MyService.java
+++ app/src/main/java/com/example/servicios/MyService.java
@@ -3,14 +3,61 @@ package com.example.servicios;
 import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
-
 public class MyService extends IntentService {
+
+    private  static  final String TAG = "PDMD";
+    private ToneGenerator _tg;
+    protected final static int DURACION = 1500;
+    public MyService() {
+        super("MyService");
+        _tg = new ToneGenerator(AudioManager.STREAM_ALARM,100);
+
+    }
+    @Override
+    public void onCreate() {
+        Log.i(TAG,"onCreate");
+    }
+    @Override
+    public void onDestroy() {
+        Log.i(TAG,"onDestroy");
+
+        super.onDestroy();
+    }
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.i(TAG,"onHandleIntent"+ intent);
+        String etiqueta = intent.getStringExtra("etiqueta");
+        play(etiqueta);
+        MainActivity.addKey(this,intent.getStringExtra("tecla"));
+    }
+    private void play(String etiqueta) {
+        int tono = 0;
+        switch (etiqueta.charAt(0)){
+            case '#': tono = ToneGenerator.TONE_DTMF_P; break;
+            case '*': tono = ToneGenerator.TONE_DTMF_S; break;
+            default: tono = ToneGenerator.TONE_DTMF_0 + (etiqueta.charAt(0) - '0'); break;
+        }
+        _tg.startTone(tono,DURACION);
+
+        try {
+            Thread.sleep(DURACION);
+        }catch (InterruptedException e){
+            throw  new RuntimeException();
+        }
+    }
+}
+}
+
+
+/*
+public class MyService extends IntentService { //ej 2
     private  static  final String TAG = "PDMD";
     private ToneGenerator _tg;
     protected final static int DURACION = 500;
@@ -53,12 +100,13 @@ public class MyService extends IntentService {
         }
     }
 }
+*/
 
 
 
 /*
 
-public class MyService extends Service {
+public class MyService extends Service { //ej 1
     private  static  final String TAG = "PDMD";
 
     public MyService() {
